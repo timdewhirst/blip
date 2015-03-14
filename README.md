57e9f6c87c7b239724a7e10ae036bf9063544911
tim dewhirst <tim@bugless.co.uk>
2015-03-14 22:10:01 +0000

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # blip
-harvest from blip
+
+A backup utility for blipfoto.com
+
+Sometimes, just sometimes, you might want to backup your blips: this does a very basic job. It will:
+* create a new directory with your username
+* starting from the most recent entry, work backwards until no previous entry is found
+* for each entry save:
+** content.json (the page content including comments, ratings and metadata)
+** image.jpg (the standard resolution image)
+
+There are some limitations:
+* I didn't figure out how to get the high-res image
+* it will sleep for 2s between fetching each entry to avoid spamming blipfoto's servers
+
+To run you'll need python3 (2 should probably work too) and it should be run as:
+
+* python3 previewer.py <username> (for me: tpd)
+
+I've only tested this on Linux; if anyone would like to test and fix on OS X or windows please do.
+
---
+++ previewer.py
@@ -0,0 +1,76 @@
+
+import os
+import os.path
+import requests
+import sys
+import json
+import time
+
+baseendpoint='https://api.polaroidblipfoto.com/4/'
+accesstoken='305411915fa8ab4f0d4bf0a3bb2e38968f0a8d1a'
+
+# need either entry_id or username
+def getData( params, basedir ):
+    params.update(
+        { 'return_details': 1,
+          'return_metadata': 1,
+          'return_comments': 1,
+          'include_replies': 1,
+          'return_related': 1
+        } )
+    
+    r=requests.get(baseendpoint+'entry', params=params, headers={'Authorization':'Bearer '+accesstoken})
+    if r.status_code != requests.codes.ok:
+        print( "failed to fetch data: {0}".format( r.status_code ) )
+        exit( 1 )
+
+    m=r.json();
+    entryid=m['data']['entry']['entry_id_str']
+    previd=m['data']['related']['previous']['entry_id_str']
+    imgurl=m['data']['entry']['image_url']
+    print( "entry id: {0}, {1}".format( entryid, imgurl ) )
+
+    savedir=os.path.join( basedir, entryid )
+    if not os.path.exists( savedir ):
+        os.mkdir( savedir )
+
+        # save the content
+        fid=open( os.path.join( savedir, "content.json" ), "w+" )
+        fid.write( r.text )
+        fid.close()
+
+        # go and get the image
+        r=requests.get(imgurl, headers={'Authorization':'Bearer '+accesstoken})
+        if r.status_code != requests.codes.ok:
+            print( "failed to fetch image: {0}".format( r.status_code ) )
+        else:
+            fid=open( os.path.join( savedir, "image.jpg" ), "wb" )
+            fid.write( r.content )
+            fid.close()
+
+        print( "saved content and image!" )
+    else:
+        print( "looks like you already have this entry; skipping..." )
+
+    if previd is None:
+        print( "no more entries" )
+        exit( 0 )
+
+    time.sleep( 2 )
+    getData( { 'entry_id': previd }, basedir )
+        
+    
+def main( argv ):
+    if len( argv ) != 2:
+        print( "usage: {0} <username>".format( argv[0] ) )
+        exit( 1 )
+
+    # okay, let's go! start from most recent and work backwards
+    username=argv[1]
+    if not os.path.exists( username ):
+        os.mkdir( username )
+        
+    getData( { 'username': username }, username )
+
+if __name__ == "__main__":
+    main( sys.argv )
